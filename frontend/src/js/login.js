1e673b20b950e76ead1ea4dd7caba28a6c042901
MALHAR BHATT <malharbhatt.devs@gmail.com>
2026-03-19 23:13:39 +0530

    PHASE : Implement Demo Account Login for recruiters.

--- frontend/src/js/login.js
+++ frontend/src/js/login.js
@@ -81,8 +81,11 @@ function setRole(role){
 
 async function login(event) {
     event.preventDefault();
-
     const errorMsg = document.getElementById("errorMsg");
+    
+    loginBtn.disabled = true;
+    btnText.textContent = "Logging in...";
+    spinner.classList.remove("hidden");
 
     const account_number = document.getElementById("account_number").value;
     const pin = document.getElementById("pin_number").value;
@@ -93,7 +96,8 @@ async function login(event) {
         const response = await fetch("http://127.0.0.1:5000/auth/login", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({account_number:account_number,
+            body: JSON.stringify({
+                account_number:account_number,
                 pin:pin,
                 role:currentRole
              })
@@ -112,12 +116,12 @@ async function login(event) {
                 const role = localStorage.getItem("role");
                 if(token && role){
                     document.body.innerHTML = `
-                    <div class="flex items-center justify-center h-screen">
-                        <div class="text-center">
-                            <p class="text-lg font-semibold">Redirecting ...</p>
-                            <div class="animate-spin mt-4">⏳</div>
+                        <div class="flex items-center justify-center h-screen">
+                            <div class="text-center">
+                                <p class="text-lg font-semibold">Redirecting ...</p>
+                                <div class="animate-spin mt-4">⏳</div>
+                            </div>
                         </div>
-                    </div>
                     `;
                     setTimeout(()=>{
                         if(role === "admin"){
@@ -133,16 +137,7 @@ async function login(event) {
                 sessionStorage.setItem("access_token", data.access_token);
                 sessionStorage.setItem("refresh_token", data.refresh_token);
                 sessionStorage.setItem("account_number", account_number);
-                sessionStorage.setItem("role",currentRole);
-                setTimeout(()=>{
-                    if(role === "admin"){
-                        window.location.href = "admin_dashboard.html";
-                    }else if(role === "employee"){
-                        window.location.href = "employee_dashboard.html";
-                    }else {
-                        window.location.href = "dashboard.html";
-                    }
-                },500);
+                sessionStorage.setItem("role", currentRole);
             }
             
         } else {
@@ -162,6 +157,87 @@ async function login(event) {
         errorMsg.textContent = "Server error. Please try again.";
         errorMsg.classList.remove("hidden");
 
+    } finally {
+        loginBtn.disabled = false;
+        btnText.textContent = "Login";
+        spinner.classList.add("hidden");
+    }
+}
+
+async function demoLogin(role){
+
+    let account_number = "";
+    let pin = "";
+    let current_role = role;
+    
+    //Demo credentials
+    if (role === "admin"){
+        account_number = "1003";
+        pin = "1234";
+    }else if(role === "employee"){
+        account_number = "1005";
+        pin = "5555";
+    }else{
+        account_number = "1002";
+        pin = "2345" ;
+    }
+
+    try{
+        const response =  await fetch("http://127.0.0.1:5000/auth/login",{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body: JSON.stringify({
+                account_number:account_number,
+                pin:pin,
+                role:current_role
+            })
+        });
+
+        const data = await response.json();
+
+        if(response.ok){
+            localStorage.setItem("access_token",data.access_token);
+            localStorage.setItem("refresh_token",data.refresh_token);
+            localStorage.setItem("account_number",account_number);
+            localStorage.setItem("role",current_role);
+
+            const token = localStorage.getItem("access_token");
+            const role = localStorage.getItem("role");
+                if(token && role){
+                    document.body.innerHTML = `
+                        <div class="flex items-center justify-center h-screen">
+                            <div class="text-center">
+                                <p class="text-lg font-semibold">Redirecting ...</p>
+                                <div class="animate-spin mt-4">⏳</div>
+                            </div>
+                        </div>
+                    `;
+                    setTimeout(()=>{
+                        if(role === "admin"){
+                            window.location.href = "admin_dashboard.html";
+                        } else if(role === "employee"){
+                            window.location.href = "employee_dashboard.html";
+                        } else {
+                            window.location.href = "dashboard.html";
+                        }
+                    },500); 
+                }
+        }
+    }catch(error){
+        console.log(error);
+        
+        const accountInput = document.getElementById("account_number");
+        const pinInput = document.getElementById("pin_number");
+
+        accountInput.classList.add("border-red-500");
+        pinInput.classList.add("border-red-500");
+
+        errorMsg.textContent = "Server error. Please try again.";
+        errorMsg.classList.remove("hidden");
+    } finally {
+        loginBtn.disabled = false;
+        btnText.textContent = "Login";
+        spinner.classList.add("hidden");
     }
 }
 
