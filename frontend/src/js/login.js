fb88d3d96ebdce69ac094ad8179e5cf09d6547b1
MALHAR BHATT <malharbhatt.devs@gmail.com>
2026-04-01 18:56:02 +0530

    commit

--- frontend/src/js/login.js
+++ frontend/src/js/login.js
@@ -126,11 +126,11 @@ async function login(event) {
                     `;
                     setTimeout(()=>{
                         if(role === "admin"){
-                            window.location.href = "admin_dashboard.html";
+                            window.location.href = "./frontend/src/admin_dashboard.html";
                         } else if(role === "employee"){
-                            window.location.href = "employee_dashboard.html";
+                            window.location.href = "./frontend/src/employee_dashboard.html";
                         } else {
-                            window.location.href = "dashboard.html";
+                            window.location.href = "./frontend/src/dashboard.html";
                         }
                     },500); 
                 }
