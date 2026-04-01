1a7c89c5881e3e1814b240ee664b74f2237c58e0
MALHAR BHATT <malharbhatt.devs@gmail.com>
2026-04-01 19:05:26 +0530

    COMMIT

--- frontend/src/js/login.js
+++ frontend/src/js/login.js
@@ -24,11 +24,11 @@ document.addEventListener("DOMContentLoaded",function(){
         `;
         setTimeout(()=>{
         if(role === "admin"){
-            window.location.href = "../frontend/src/admin_dashboard.html";
+            window.location.href = "bankingcore/frontend/src/admin_dashboard.html";
         } else if(role === "employee"){
-            window.location.href = "../frontend/src/employee_dashboard.html";
+            window.location.href = "bankingcore/frontend/src/employee_dashboard.html";
         } else {
-            window.location.href = "../frontend/src/dashboard.html";
+            window.location.href = "bankingcore/frontend/src/dashboard.html";
         }
     },500); 
     }
@@ -126,11 +126,11 @@ async function login(event) {
                     `;
                     setTimeout(()=>{
                         if(role === "admin"){
-                            window.location.href = "../frontend/src/admin_dashboard.html";
+                            window.location.href = "bankingcore/frontend/src/admin_dashboard.html";
                         } else if(role === "employee"){
-                            window.location.href = "../frontend/src/employee_dashboard.html";
+                            window.location.href = "bankingcore/frontend/src/employee_dashboard.html";
                         } else {
-                            window.location.href = "../frontend/src/dashboard.html";
+                            window.location.href = "bankingcore/frontend/src/dashboard.html";
                         }
                     },500); 
                 }
