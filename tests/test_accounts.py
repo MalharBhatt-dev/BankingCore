1c1ea2775f5308551d0686381ad2d750c65a2003
MALHAR BHATT <malharbhatt.devs@gmail.com>
2026-04-02 00:01:14 +0530

    modify the test.yml for dynamic test cases.

--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -8,20 +8,20 @@ def test_create_account(client):
 
     assert res.status_code == 201
 
-def test_get_balance(client,auth_token):
-    res = client.get("/accounts/1002",headers={
+def test_get_balance(client,auth_token,account_number):
+    res = client.get(f"/accounts/{account_number}",headers={
         "Authorization":f"Bearer {auth_token}"
     })
 
     assert res.status_code == 200
     assert "balance" in res.get_json()
 
-def test_deposit(client,auth_token):
-    res = client.post("/accounts/1002/deposit",json={"amount":200},headers={"Authorization":f"Bearer {auth_token}"})
+def test_deposit(client,auth_token,account_number):
+    res = client.post(f"/accounts/{account_number}/deposit",json={"amount":200},headers={"Authorization":f"Bearer {auth_token}"})
 
     assert res.status_code == 200
 
-def test_withdraw(client,auth_token):
-    res = client.post("/accounts/1002/withdraw",json={"amount":200},headers={"Authorization":f"Bearer {auth_token}"})
+def test_withdraw(client,auth_token,account_number):
+    res = client.post(f"/accounts/{account_number}/withdraw",json={"amount":200},headers={"Authorization":f"Bearer {auth_token}"})
 
     assert res.status_code == 200
--- tests/test_transactions.py
+++ tests/test_transactions.py
@@ -1,4 +1,4 @@
-def test_transfer(client,auth_token):
+def test_transfer(client,auth_token,account_number):
     #create account
     client.post("/accounts",json={
         "name":"Receiver",
@@ -7,12 +7,12 @@ def test_transfer(client,auth_token):
         "initial_deposit":500
     })
 
-    res = client.post("/accounts/1002/transfer",json={"to_account":1005,"amount":100},headers={"Authorization":f"Bearer {auth_token}"})
+    res = client.post(f"/accounts/{account_number}/transfer",json={"to_account":1005,"amount":100},headers={"Authorization":f"Bearer {auth_token}"})
 
     assert res.status_code == 200
 
-def test_transaction_histroy(client,auth_token):
-    res = client.get("/accounts/1002/transactions",headers={"Authorization":f"Bearer {auth_token}"})
+def test_transaction_histroy(client,auth_token,account_number):
+    res = client.get(f"/accounts/{account_number}/transactions",headers={"Authorization":f"Bearer {auth_token}"})
 
     assert res.status_code == 200
     assert "transactions" in res.get_json()
