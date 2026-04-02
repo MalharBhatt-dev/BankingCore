da5bd829ce659bf3f55fa529c57ad8e38ebd81a5
MALHAR BHATT <malharbhatt.devs@gmail.com>
2026-04-02 12:25:11 +0530

    resolve test_service_requests

--- tests/test_service_requests.py
+++ tests/test_service_requests.py
@@ -1,7 +1,11 @@
-def test_create_request(client,auth_headers):
-    res = client.post("/requests",json={"query_type":"CHANGE_PIN","description":"Need to change pin"},headers=auth_headers)
+def test_create_request(client,create_user,login_user):
+    acc=create_user()
+    auth_token=login_user(acc)["access_token"]
+    res = client.post("/requests",json={"query_type":"CHANGE_PIN","description":"Need to change pin"},headers={"Authorization":f"Bearer {auth_token}"})
     assert res.status_code == 201
 
-def test_get_my_requests(client,auth_headers):
-    res = client.get("/requests/my",headers=auth_headers)
+def test_get_my_requests(client,create_user,login_user):
+    acc=create_user()
+    auth_token=login_user(acc)["access_token"]
+    res = client.get("/requests/my",headers={"Authorization":f"Bearer {auth_token}"})
     assert res.status_code == 200
