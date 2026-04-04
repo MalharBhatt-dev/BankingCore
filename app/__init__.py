3e02b741ce406367ecb05e3b7c24fcc6eb4bfa28
MALHAR BHATT <malharbhatt.devs@gmail.com>
2026-04-04 23:24:25 +0530

    resolve the init file

--- app/__init__.py
+++ app/__init__.py
@@ -12,51 +12,61 @@ import logging
 from logging.handlers import RotatingFileHandler
 
 def create_app(config_class=Config):
-    app = Flask(__name__,static_folder="static",template_folder="templates")
-   
-    CORS(app)
-    limiter.init_app(app)
-    app.config.from_object(config_class)
-    db.init_app(app)
-    migrate.init_app(app,db)
-    #dependency injection
-    repo = AccountRepository()
-    service = BankingServices(repo,app.config["ADMIN_KEY"],app.logger)
-    app.config["service"] = service
+    print("starting app...")
+    try:
+        app = Flask(__name__,static_folder="static",template_folder="templates")
+        print("Flask created")
+    
+        CORS(app)
+        limiter.init_app(app)
+        app.config.from_object(config_class)
+        print("config loaded")
+        db.init_app(app)
+        print("db inititaled.")
+        migrate.init_app(app,db)
+        print("migration completed.")
+        #dependency injection
+        repo = AccountRepository()
+        service = BankingServices(repo,app.config["ADMIN_KEY"],app.logger)
+        app.config["service"] = service
 
-    #dependecy injection for service_request
-    request_repo = ServiceRequestRepository()
-    request_service = ServiceRequestService(request_repo,app.logger)
-    app.config["request_service"] = request_service
+        #dependecy injection for service_request
+        request_repo = ServiceRequestRepository()
+        request_service = ServiceRequestService(request_repo,app.logger)
+        app.config["request_service"] = request_service
 
-    #loggin setup
-    formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
+        #loggin setup
+        formatter = logging.Formatter("%(asctime)s | %(levelname)s | %(message)s")
 
-    if not app.logger.handlers:
-        if not app.config.get("TESTING"):
-            log_dir = os.path.dirname(app.config["LOG_FILE"])
-            if log_dir :
-                os.makedirs(log_dir,exist_ok=True)
-            handler = RotatingFileHandler(app.config.get("LOG_FILE","banking_core.log"),maxBytes=10240,backupCount=5)
-        else : 
-            handler = logging.StreamHandler()
+        if not app.logger.handlers:
+            if not app.config.get("TESTING"):
+                log_dir = os.path.dirname(app.config["LOG_FILE"])
+                if log_dir :
+                    os.makedirs(log_dir,exist_ok=True)
+                handler = RotatingFileHandler(app.config.get("LOG_FILE","banking_core.log"),maxBytes=10240,backupCount=5)
+            else : 
+                handler = logging.StreamHandler()
 
-        handler.setFormatter(formatter)
-        app.logger.addHandler(handler)
-        app.logger.setLevel(app.config["LOG_LEVEL"])
+            handler.setFormatter(formatter)
+            app.logger.addHandler(handler)
+            app.logger.setLevel(app.config["LOG_LEVEL"])
 
-    csp={
-        "default-src":["'self'"],
-        "script-src":["'self'","'unsafe-inline'"],
-        "style-src":["'self'","'unsafe-inline'"]
-        }
+        csp={
+            "default-src":["'self'"],
+            "script-src":["'self'","'unsafe-inline'"],
+            "style-src":["'self'","'unsafe-inline'"]
+            }
 
-    Talisman(app,content_security_policy=csp,force_https=False)
-    #register routes and errors
-    from app.routes import register_routes
-    from app.errors import register_error_handlers
+        Talisman(app,content_security_policy=csp,force_https=False)
+        #register routes and errors
+        from app.routes import register_routes
+        from app.errors import register_error_handlers
 
-    register_routes(app)
-    register_error_handlers(app)
+        register_routes(app)
+        register_error_handlers(app)
 
-    return app
+        return app
+    
+    except Exception as e:
+        print("Crash",str(e))
+        raise
