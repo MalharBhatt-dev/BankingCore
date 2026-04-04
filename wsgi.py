092fc3d00dfa42d5e4eefd1238a998d42a2c3051
MALHAR BHATT <malharbhatt.devs@gmail.com>
2026-04-04 22:32:13 +0530

    REOLVE THE WSGI FILE

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 import os
 from app import create_app
 app = create_app()
-if __name__ == "__main__":
-    port=int(os.environ.get("POST",5000))
-    app.run(host="0.0.0.0",port=port)
+# if __name__ == "__main__":
+#     port=int(os.environ.get("POST",5000))
+#     app.run(host="0.0.0.0",port=port)
